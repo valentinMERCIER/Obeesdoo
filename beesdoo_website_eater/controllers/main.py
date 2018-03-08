66bdfb8b912333f961b360adc1c099c1c400a3b4
Rémy Taymans <remytaymans@gmail.com>
2018-03-08 17:08:46 +0100

    [ADD] b_website_eater: Information about eaters
    Add the eater information in the website portal.

---
+++ beesdoo_website_eater/controllers/main.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2015-2016 Odoo S.A.
+# Copyright 2016 Jairo Llopis <jairo.llopis@tecnativa.com>
+# Copyright 2018 Rémy Taymans <remytaymans@gmail.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+
+from openerp.http import request
+
+from openerp.addons.website_portal_v10.controllers.main import WebsiteAccount
+
+
+class EaterWebsiteAccount(WebsiteAccount):
+
+    def _prepare_portal_layout_values(self):
+        values = super(EaterWebsiteAccount,
+                       self)._prepare_portal_layout_values()
+        partner = request.env.user.partner_id.commercial_partner_id
+        values.update({
+            'eaters': partner.child_eater_ids,
+        })
+        return values
